45e4ee84373f370a9689f4941b8dc6b21ead9866
Andy <andygrecu2000@gmail.com>
2020-11-30 17:34:00 +0200

    sign in

--- src/App.js
+++ src/App.js
@@ -1,14 +1,193 @@
-import React, { Component } from 'react';
+import './App.css';
 
-
-class App extends Component {
-  render() {
-    return (
-      <div>
-        
+function App() {
+  return (
+    
+    <body>
+        <div id="Index">
+        <svg class="Ellipse_12">
+		<ellipse id="Ellipse_12" rx="316" ry="316" cx="316" cy="316">
+		</ellipse>
+	</svg>
+  <svg class="Polygon_1" viewBox="0 0 721 579">
+		<path id="Polygon_1" d="M 360.4999694824219 0 L 721 579 L 0 579 Z">
+		</path>
+	</svg>
+	<svg class="Rectangle_8">
+		<rect id="Rectangle_8" rx="40" ry="40" x="0" y="0" width="1725" height="890">
+		</rect>
+	</svg>
+	<svg class="Rectangle_9_l">
+		<linearGradient id="Rectangle_9_l" spreadMethod="pad" x1="0.877" x2="0.419" y1="1.019" y2="0">
+			<stop offset="0" stop-color="#b821d5" stop-opacity="0.722"></stop>
+			<stop offset="1" stop-color="#1a116b" stop-opacity="0.82"></stop>
+		</linearGradient>
+		<rect id="Rectangle_9_l" rx="40" ry="40" x="0" y="0" width="632" height="890">
+		</rect>
+	</svg>
+  <svg class="Ellipse_13">
+		<ellipse id="Ellipse_13" rx="316" ry="316" cx="316" cy="316">
+		</ellipse>
+	</svg>
+  <div id="Welcome_singi_n">
+		<div id="Welcome_Back">
+			<span>Welcome Back!</span>
+		</div>
+		<div id="To_keep_connected_with_us_plea">
+			<span>To keep connected with us please login with your personal info</span>
+		</div>
+		<svg class="Rectangle_14">
+			<rect onclick="application.goToTargetView(event)" id="Rectangle_14" rx="42.5" ry="42.5" x="0" y="0" width="345" height="85">
+			</rect>
+		</svg>
+		<div id="SIGN_IN">
+			<span>SIGN IN</span>
+		</div>
+	</div>
+  <div id="Create_account">
+  <div id="Create_Account">
+  <span>Create Account</span>
+  </div>
+  <svg class="Ellipse_14">
+			<ellipse id="Ellipse_14" rx="50" ry="50" cx="50" cy="50">
+			</ellipse>
+		</svg>
+    <svg class="Ellipse_15">
+			<ellipse id="Ellipse_15" rx="50" ry="50" cx="50" cy="50">
+			</ellipse>
+		</svg>
+    <svg class="Ellipse_16">
+			<ellipse id="Ellipse_16" rx="50" ry="50" cx="50" cy="50">
+			</ellipse>
+		</svg>
+    <img id="png-clipart-google-logo-comput" src="png-clipart-google-logo-comput.png" srcset="png-clipart-google-logo-comput.png 1x, png-clipart-google-logo-comput@2x.png 2x"alt="google+"></img>
+		<img id="facebook-logo-svg-png-icon-fre" src="facebook-logo-svg-png-icon-fre.png" srcset="facebook-logo-svg-png-icon-fre.png 1x, facebook-logo-svg-png-icon-fre@2x.png 2x"alt="facebok"></img>
+		<img id="linkedin-icon-black-linkedin-l" src="linkedin-icon-black-linkedin-l.png" srcset="linkedin-icon-black-linkedin-l.png 1x, linkedin-icon-black-linkedin-l@2x.png 2x"alt="linkedin"></img>
+    <svg class="Rectangle_10">
+			<rect id="Rectangle_10" rx="0" ry="0" x="0" y="0" width="545" height="71">
+			</rect>
+      
+		</svg>
+		<svg class="Rectangle_11">
+			<rect id="Rectangle_11" rx="0" ry="0" x="0" y="0" width="545" height="71">
+			</rect>
+		</svg>
+		<svg class="Rectangle_12">
+			<rect id="Rectangle_12" rx="0" ry="0" x="0" y="0" width="545" height="71">
+			</rect>
+		</svg>
+		<svg class="Rectangle_13">
+			<rect id="Rectangle_13" rx="42.5" ry="42.5" x="0" y="0" width="345" height="85">
+			</rect>
+		</svg>
+		<div id="SIGN_UP">
+			<span>SIGN UP</span>
+		</div>
+		<div id="or_use_your_email_for_registra">
+			<span>or use your email for registration:</span>
+		</div>
+		<div id="Name">
+			<span>Name</span>
+		</div>
+		<div id="Email">
+			<span>Email</span>
+		</div>
+		<div id="Password">
+			<span>Password</span>
       </div>
-    );
-  }
+  </div>
+        </div>
+        <div id="Index__1">
+	<svg class="Ellipse_12_bb">
+		<ellipse id="Ellipse_12_bb" rx="316" ry="316" cx="316" cy="316">
+		</ellipse>
+	</svg>
+	<svg class="Polygon_1_bc" viewBox="0 0 721 579">
+		<path id="Polygon_1_bc" d="M 360.4999694824219 0 L 721 579 L 0 579 Z">
+		</path>
+	</svg>
+	<svg class="Rectangle_8_bd">
+		<rect id="Rectangle_8_bd" rx="40" ry="40" x="0" y="0" width="1725" height="890">
+		</rect>
+	</svg>
+	<svg class="Rectangle_9_bf">
+		<linearGradient id="Rectangle_9_bf" spreadMethod="pad" x1="0.877" x2="0.419" y1="1.019" y2="0">
+			<stop offset="0" stop-color="#b821d5" stop-opacity="0.722"></stop>
+			<stop offset="1" stop-color="#1a116b" stop-opacity="0.82"></stop>
+		</linearGradient>
+		<rect id="Rectangle_9_bf" rx="40" ry="40" x="0" y="0" width="632" height="890">
+		</rect>
+	</svg>
+	<svg class="Ellipse_13_bg">
+		<ellipse id="Ellipse_13_bg" rx="316" ry="316" cx="316" cy="316">
+		</ellipse>
+	</svg>
+	<div id="Welcome_singi_n_bh">
+		<div id="Greetings">
+			<span>Greetings!</span>
+		</div>
+		<div id="Enter_your_personal_details_an">
+			<span>Enter your personal details and journey with us</span>
+		</div>
+		<svg class="Rectangle_14_bk">
+			<rect onclick="application.goToTargetView(event)" id="Rectangle_14_bk" rx="42.5" ry="42.5" x="0" y="0" width="345" height="85">
+			</rect>
+		</svg>
+		<div id="SIGN_UP_bl">
+			<span>SIGN UP</span>
+		</div>
+	</div>
+	<div id="Create_account_bm">
+		<div id="Sign_in">
+			<span>Sign in</span>
+		</div>
+		<svg class="Ellipse_14_bo">
+			<ellipse id="Ellipse_14_bo" rx="50" ry="50" cx="50" cy="50">
+			</ellipse>
+		</svg>
+		<svg class="Ellipse_15_bp">
+			<ellipse id="Ellipse_15_bp" rx="50" ry="50" cx="50" cy="50">
+			</ellipse>
+		</svg>
+		<svg class="Ellipse_16_bq">
+			<ellipse id="Ellipse_16_bq" rx="50" ry="50" cx="50" cy="50">
+			</ellipse>
+		</svg>
+		<img id="png-clipart-google-logo-comput_br" src="png-clipart-google-logo-comput_br.png" srcset="png-clipart-google-logo-comput_br.png 1x, png-clipart-google-logo-comput_br@2x.png 2x"alt="google+"></img>
+			
+		<img id="facebook-logo-svg-png-icon-fre_bs" src="facebook-logo-svg-png-icon-fre_bs.png" srcset="facebook-logo-svg-png-icon-fre_bs.png 1x, facebook-logo-svg-png-icon-fre_bs@2x.png 2x"alt="facebook"></img>
+			
+		<img id="linkedin-icon-black-linkedin-l_bt" src="linkedin-icon-black-linkedin-l_bt.png" srcset="linkedin-icon-black-linkedin-l_bt.png 1x, linkedin-icon-black-linkedin-l_bt@2x.png 2x"alt="linkedin"></img>
+			
+		<svg class="Rectangle_10_bu">
+			<rect id="Rectangle_10_bu" rx="0" ry="0" x="0" y="0" width="545" height="71">
+			</rect>
+		</svg>
+		<svg class="Rectangle_11_bv">
+			<rect id="Rectangle_11_bv" rx="0" ry="0" x="0" y="0" width="545" height="71">
+			</rect>
+		</svg>
+		<svg class="Rectangle_13_bw">
+			<rect id="Rectangle_13_bw" rx="42.5" ry="42.5" x="0" y="0" width="345" height="85">
+			</rect>
+		</svg>
+		<div id="SIGN_IN_bx">
+			<span>SIGN IN</span>
+		</div>
+		<div id="or_use_your_email_and_password">
+			<span>or use your email and password to login:</span>
+		</div>
+		<div id="Email_bz">
+			<span>Email</span>
+		</div>
+		<div id="Password_b">
+			<span>Password</span>
+		</div>
+	</div>
+</div>
+    </body>
+    
+  );
 }
 
 export default App;
