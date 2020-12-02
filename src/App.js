9c19ed1918a48eae20bd79bb333058680bcd81f9
SebastianDochia <sebastiam.dochia@gmail.com>
2020-12-02 18:09:11 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
 
 import Auxiliary from './hoc/Auxiliary/Auxiliary';
 import LoginPage from './Containers/LoginPage/LoginPage';
+import MainPage from './Containers/MainPage/MainPage';
 
 class App extends Component {
 	render() {
 		return (
 			<Auxiliary>
-				<LoginPage />
+				<BrowserRouter>
+					<Route path='/' exact component={LoginPage} />
+					<Route path='/main-page' exact component={MainPage} />
+				</BrowserRouter>
 			</Auxiliary>
 		);
 	}
---
+++ src/Containers/MainPage/MainPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class MainPage extends Component {
+    render () {
+        return (
+            <p>I am main page</p>
+        );
+    }
+}
+
+export default MainPage;
