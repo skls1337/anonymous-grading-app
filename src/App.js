4c4db21ae795aa831a11f85fabbaf17fafb8e202
Andy <andygrecu2000@gmail.com>
2020-12-03 03:07:45 +0200

    coommit

--- src/App.js
+++ src/App.js
@@ -3,29 +3,17 @@ import { Route, BrowserRouter } from 'react-router-dom';
 
 import Auxiliary from './hoc/Auxiliary/Auxiliary';
 import LoginPage from './Containers/LoginPage/LoginPage';
-<<<<<<< HEAD
 import MainPage from './Containers/MainPage/MainPage';
-=======
 import Background from './Containers/LoginPage/Background';
->>>>>>> brenciu-lu-andy-dreq
 
 class App extends Component {
 	render() {
 		return (
 			<Auxiliary>
-<<<<<<< HEAD
 				<BrowserRouter>
-					<Route path='/main-page' exact component={LoginPage} />
+					<Route path='/main-page' exact component={Background} />
 					<Route path='/' exact component={MainPage} />
 				</BrowserRouter>
-=======
-				
-		
-				<Background/>
-				
-				
-				
->>>>>>> brenciu-lu-andy-dreq
 			</Auxiliary>
 		);
 	}
