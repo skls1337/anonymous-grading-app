81c24907461abc30713c2d88f2d2fd7db25865d8
DombleJ <jipadragos09@gmail.com>
2021-01-13 16:18:07 +0200

    Revert "Merge branch 'master' of https://github.com/skls1337/anonymous-grading-app into master"
    This reverts commit 845a0a0e86c3c818fa6d36b2fe560af0cc3c3b1b, reversing
    changes made to c3ad326035c7fbc47b84bb4b53032642e67585a4.

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,9 @@ const fileupload = require('express-fileupload');
 const morgan = require('morgan');
 const cookieparser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
-const cors=require('cors')
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
+
 // Load config.env
 dotenv.config({ path: './config/config.env'});
 
@@ -24,9 +24,6 @@ const app = express();
 
 // body parser
 app.use(express.json());
-
-//cors
-app.use(cors());
 
 // Cookie-parser
 app.use(cookieparser());
@@ -52,7 +49,7 @@ app.use('/api/v1/auth', auth);
 
 app.use(errorHandler);
 
-const PORT=process.env.PORT || 3001;
+const PORT=process.env.PORT || 3000;
 
 const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.magenta.bold.underline));
 
--- frontend/src/Containers/LoginPage/Background.js
+++ frontend/src/Containers/LoginPage/Background.js
@@ -1,21 +1,16 @@
 import React,{Component} from 'react'
 import { Route, Switch } from 'react-router'
 import classes from './Background.css'
-import ForgotPassword from './ForgotPassword'
-import ResetPassword from './ResetPassword'
 import LoginPage from './LoginPage'
 import RegisterPage from './RegisterPage'
-
 class Background extends Component{
     render(){
      return(
 
         <div className={classes.Background}>
             <Switch>
-                <Route path="/start/login" component={()=><LoginPage setUser={this.props.setUser}/>}/>
+                <Route path="/start/login" component={LoginPage}/>
                 <Route path="/start/register" component={RegisterPage}/>
-                <Route path="/start/forgotpassword" component={ForgotPassword}/>
-                <Route path="/start/resetpassword/:id" component={ResetPassword}/>
             </Switch>
         </div>
      )   
