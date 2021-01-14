80318fcddd48299f74790465e2b7fc1879652dbf
Andy <andygrecu2000@gmail.com>
2021-01-14 13:17:36 +0200

    in progress

--- frontend/src/Containers/Reviews/ReviewProjects.js
+++ frontend/src/Containers/Reviews/ReviewProjects.js
@@ -1,28 +1,40 @@
 import React, { Component } from 'react'
 import classes from '../../Components/MainPage/SubPage/Subpage.css'
+import { Link } from 'react-router-dom';
 class ReviewProjects extends Component {
-    titles = []
+    onClickHandler=(id)=>{
+       
+      <Link to={"/start/login"}/>
+    
+    }
     render() {
+        const projects = []
         try {
 
             for (let i = 0; i < this.props.projects.count; i++) {
-                this.titles.push(this.props.projects.data[i].title);
+                
+               projects.push({"id":this.props.projects.data[i]._id,"title":this.props.projects.data[i].title})
+             
             }
-            this.titles.map(el => {
-                console.log(el)
-            })
         } catch (err) {
             console.log(err);
         }
-        const items = []
-        for (const [index, value] of this.titles.entries()) {
-            items.push(<p key={index}>{value}</p>)
+        const items = [] ;
+        for (let i=0;i<projects.length;i++) {
+            items.push(
+            <p style={{marginLeft:250}}>
+                 <button style={{width:100,height:30,marginRight:200,borderRadius:25,borderColor:"#2B7A78"}} onClick={()=>this.onClickHandler(projects[i].id)}>Review</button>   
+                {projects[i].title}
+                
+            </p>)
           }
         return (
-            <div className={classes.Subpage}>
+          
+            <div className={classes.SubRev}>
+                
                {items}
             </div>
-
+           
 
 
 
