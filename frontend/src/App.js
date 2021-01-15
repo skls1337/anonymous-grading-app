cda30922d8deb423dd1f3eb45f93d36746c3124e
SebastianDochia <44543115+SebastianDochia@users.noreply.github.com>
2021-01-15 13:17:56 +0200

    fixed versions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
-import React, { Component, } from 'react';
+import React, { Component } from 'react';
 import { Route, BrowserRouter, Redirect } from 'react-router-dom';
 
 import Auxiliary from './hoc/Auxiliary/Auxiliary';
 import MainPage from './Containers/MainPage/MainPage';
 import Background from './Containers/LoginPage/Background';
-import axios from 'axios';
+
 
 class App extends Component {
 	state = {
+		isUserAuth: true
 	}
 	data = [];
 	userAuthHandler = () => {
--- frontend/src/Components/MainPage/SubPage/SubPage.js
+++ frontend/src/Components/MainPage/SubPage/SubPage.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
 
 import Navbar2 from '../Navbar2/Navbar2';
 import classes from './SubPage.css';
