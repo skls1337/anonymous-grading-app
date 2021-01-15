2aab2fdd5e224e8dbf816e4f024e69a151c90c95
SebastianDochia <44543115+SebastianDochia@users.noreply.github.com>
2021-01-15 16:02:15 +0200

    Created the Project form with working css

--- frontend/src/Components/MainPage/SubPageProject/SubPageProject.js
+++ frontend/src/Components/MainPage/SubPageProject/SubPageProject.js
@@ -1,13 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import classes from '../SubPage/SubPage.css';
+import classes from './SubPageProject.css';
+import subPageClasses from '../SubPage/SubPage.css';
+import barClasses from '../Navbar2/Navbar2.css';
 
-const subPageProject = () => {
-    return(
-        <div className={classes.SubPage}>
-            <p>Nothing here</p>
-        </div>
-    );
+class ProjectPage extends Component {
+    state = {
+        projectName: '',
+        shortDescription: '',
+        fullDescription: ''
+    }
+
+    handleProjectNameChange = (event) => {
+        this.setState({projectName: event.targe.value});
+    }
+
+    handleShortDescriptionChange = (event) => {
+        this.setState({shortDescription: event.target.value});
+    }
+
+    handleFullDescriptionChange = (event) => {
+        this.setState({fullDescription: event.target.value});
+    }
+
+    handleSubmit = () => {
+        console.log(this);
+    }
+
+    render() {
+        return (
+            <div className={subPageClasses.SubPage}>
+                <div className={barClasses.Navbar2}></div>
+                <div className={classes.SubPageProject}>
+                    <h1>Submit Your Project</h1>
+                    <form>
+                        <label>
+                            <p>Enter Project Name</p>
+                            <input type='text' maxLenght='30' value={this.state.projectName} onChange={this.handleProjectNameChange} />
+                        </label>
+                        <label>
+                            <p>Enter A Short Description</p>
+                            <textarea type='text' maxLength='250' value={this.state.shortDescription} onChange={this.handleShortDescriptionChange} />
+                        </label>
+                        <label>
+                            <p>Enter The Full Description Of You Project</p>
+                            <textarea type='text' maxLength='2650' value={this.state.fullDescription} onChange={this.handleFullDescriptionChange} style={{height: '600px'}} />
+                        </label>
+                    </form>
+                    <button onClick={this.handleSubmit}>SUBMIT</button>
+                </div>
+            </div>
+        );
+    }
 }
 
-export default subPageProject;
+export default ProjectPage;
--- frontend/src/Containers/ProjectPage/ProjectPage.js
+++ frontend/src/Containers/ProjectPage/ProjectPage.js
@@ -1,55 +1,17 @@
 import React, { Component } from 'react';
 
-import classes from '../ProjectPage/ProjectPage.css';
-import subPageClasses from '../../Components/MainPage/SubPage/SubPage.css';
-import barClasses from '../../Components/MainPage/Navbar2/Navbar2.css';
+import classes from '../ProfilePage/ProfilePage.css';
+import Auxiliary from '../../hoc/Auxiliary/Auxiliary';
+import SubPage from '../../Components/MainPage/SubPageProject/SubPageProject';
 
 class ProjectPage extends Component {
-    state = {
-        projectName: '',
-        shortDescription: '',
-        fullDescription: ''
-    }
-
-    handleProjectNameChange = (event) => {
-        this.setState({projectName: event.targe.value});
-    }
-
-    handleShortDescriptionChange = (event) => {
-        this.setState({shortDescription: event.target.value});
-    }
-
-    handleFullDescriptionChange = (event) => {
-        this.setState({fullDescription: event.target.value});
-    }
-
-    handleSubmit = () => {
-        console.log(this);
-    }
-
     render() {
         return (
-            <div className={subPageClasses.SubPage}>
-                <div className={barClasses.Navbar2}></div>
-                <div className={classes.SubPageProject}>
-                    <h1>Submit Your Project</h1>
-                    <form>
-                        <label>
-                            <p>Enter Project Name</p>
-                            <input type='text' maxLenght='30' value={this.state.projectName} onChange={this.handleProjectNameChange} />
-                        </label>
-                        <label>
-                            <p>Enter A Short Description</p>
-                            <textarea type='text' maxLength='250' value={this.state.shortDescription} onChange={this.handleShortDescriptionChange} />
-                        </label>
-                        <label>
-                            <p>Enter The Full Description Of You Project</p>
-                            <textarea type='text' maxLength='2650' value={this.state.fullDescription} onChange={this.handleFullDescriptionChange} style={{height: '600px'}} />
-                        </label>
-                    </form>
-                    <button onClick={this.handleSubmit}>SUBMIT</button>
+            <Auxiliary>
+                <div className={classes.ProfilePage}>
+                    <SubPage />
                 </div>
-            </div>
+            </Auxiliary>
         );
     }
 }
