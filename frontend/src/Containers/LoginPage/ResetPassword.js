51a50f05d05c5054bc8334c3f8b04919a607995f
SebastianDochia <44543115+SebastianDochia@users.noreply.github.com>
2021-01-19 10:59:21 +0200

    bogdixing and formatting

--- frontend/src/Containers/LoginPage/ResetPassword.js
+++ frontend/src/Containers/LoginPage/ResetPassword.js
@@ -1,58 +1,58 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import classes from './ResetPassword.css';
 import axios from 'axios'
 import { Redirect } from 'react-router';
 
-class ResetPassword extends Component{
-  state={}
+class ResetPassword extends Component {
+    state = {}
 
-    handleSubmit=e=>{
+    handleSubmit = e => {
         e.preventDefault()
-        const data={
-            token:this.props.match.params.id,
-            password:this.pass,
-         
+        const data = {
+            token: this.props.match.params.id,
+            password: this.pass,
+
         }
-        axios.put('http://localhost:3001/api/v1/auth/resetpassword/'+data.token,data).then(res=>{
+        axios.put('http://localhost:3001/api/v1/auth/resetpassword/' + data.token, data).then(res => {
             console.log(res);
             this.setState({
-                reset:true})
-        }).catch(err=>{console.log(err);})
+                reset: true
+            })
+        }).catch(err => { console.log(err); })
 
     }
-  
-    render(){
-        if(this.state.reset){
-            return <Redirect to='/start/login'/>
+
+    render() {
+        if (this.state.reset) {
+            return <Redirect to='/start/login' />
         }
-        return(
-        <div>
-            <p className={classes.titlu}>Reset your password? </p>
-            <p className={classes.desc}>Enter your new password</p>
-            <div className={classes.DrGri}>
-            <input
-                className={classes.inputReg} 
-                placeholder="Password"
-                type="password"
-                onChange={(event) => this.pass=event.target.value}
-            />
-             </div>
-             <div className={classes.DrGri}>
-            <input
-                className={classes.inputReg} 
-                 type="password"
-                placeholder="Password"
-                 placeholder="Confirm password"
-                onChange={(event) => this.confpass=event.target.value}
-            />
-            </div>
-            <div  className={classes.centerButton}>
-            <button className={classes.ResetButton}
-            onClick={this.handleSubmit}>
-                  CHANGE PASSWORD
+        return (
+            <div>
+                <p className={classes.titlu}>Reset your password? </p>
+                <p className={classes.desc}>Enter your new password</p>
+                <div className={classes.DrGri}>
+                    <input
+                        className={classes.inputReg}
+                        placeholder="Password"
+                        type="password"
+                        onChange={(event) => this.pass = event.target.value}
+                    />
+                </div>
+                <div className={classes.DrGri}>
+                    <input
+                        className={classes.inputReg}
+                        type="password"
+                        placeholder="Confirm password"
+                        onChange={(event) => this.confpass = event.target.value}
+                    />
+                </div>
+                <div className={classes.centerButton}>
+                    <button className={classes.ResetButton}
+                        onClick={this.handleSubmit}>
+                        CHANGE PASSWORD
             </button>
+                </div>
             </div>
-        </div>        
 
         )
     }
